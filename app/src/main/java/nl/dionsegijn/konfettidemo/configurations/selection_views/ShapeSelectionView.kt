021116c89b12ee3819ca29a8820cdd8a5ae3511f
Dion Segijn <dion@blendle.com>
2017-05-21 21:02:09 +0200

    Add view to enable/disable shapes

--- app/src/main/java/nl/dionsegijn/konfettidemo/configurations/selection_views/ShapeSelectionView.kt
+++ app/src/main/java/nl/dionsegijn/konfettidemo/configurations/selection_views/ShapeSelectionView.kt
@@ -1,6 +1,7 @@
 package nl.dionsegijn.konfettidemo.configurations.selection_views
 
 import android.content.Context
+import android.view.Gravity
 import android.widget.LinearLayout
 import nl.dionsegijn.konfettidemo.R
 
@@ -11,8 +12,8 @@ class ShapeSelectionView(context: Context?) : LinearLayout(context) {
 
     init {
         inflate(context, R.layout.view_section_shape_selection, this)
-        orientation = HORIZONTAL
+        orientation = VERTICAL
+        gravity = Gravity.CENTER_HORIZONTAL
     }
 
-    
 }
