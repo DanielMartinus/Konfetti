a7250c377385157a150342749ab477d661decfce
Dion Segijn <dion@blendle.com>
2017-05-21 13:09:15 +0200

    Cleanup demo project

--- app/src/main/java/nl/dionsegijn/konfettidemo/MainActivity.kt
+++ app/src/main/java/nl/dionsegijn/konfettidemo/MainActivity.kt
@@ -1,143 +1,31 @@
 package nl.dionsegijn.konfettidemo
 
 import android.os.Bundle
-import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
-import android.view.MotionEvent
-import android.view.ViewTreeObserver
-import android.widget.SeekBar
-import nl.dionsegijn.konfetti.KonfettiView
-import nl.dionsegijn.konfetti.models.Shape
-import nl.dionsegijn.konfetti.models.Size
-import java.util.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 /**
  * Created by dionsegijn on 3/25/17.
  */
 class MainActivity : AppCompatActivity() {
 
-    var timer: Int = 20
-
-    lateinit var konfetti: KonfettiView
-    lateinit var seekbarX: SeekBar
-    lateinit var seekbarY: SeekBar
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        konfetti = findViewById(R.id.konfetti) as KonfettiView
-        konfetti.viewTreeObserver.addOnPreDrawListener(object : ViewTreeObserver.OnPreDrawListener {
-            override fun onPreDraw(): Boolean {
-                konfetti.viewTreeObserver.removeOnPreDrawListener(this)
-                startConfetti()
-                return false
-            }
-        })
-
-        seekbarX = findViewById(R.id.velocityX) as SeekBar
-        seekbarX.progress = timer
-        seekbarX.setOnSeekBarChangeListener(getTimerSeekBarChangeListener())
-
-        seekbarY = findViewById(R.id.velocityY) as SeekBar
-        seekbarY.progress = 0
-        seekbarY.setOnSeekBarChangeListener(getWindSeekBarChangeListener())
-
-        velocityTest()
+        setupSectionButtons()
     }
 
-    fun startConfetti() {
-        val colors = intArrayOf(color(R.color.confetti1), color(R.color.confetti2), color(R.color.confetti3), color(R.color.confetti4))
-        konfetti.build()
-                .addColors(*colors)
-                .setDirection(0.0, 359.0)
-                .setSpeed(1f, 5f)
-                .setFadeOutEnabled(true)
-                .setTimeToLive(2000L)
-                .addShapes(Shape.RECT, Shape.CIRCLE)
-                .addSizes(Size.SMALL)
-                .setPosition(-50f, konfetti.width + 50f, -50f, -50f)
-                .stream(300, 5000L)
-    }
-
-    var startX: Float = 0f
-    var startY: Float = 0f
-    var speed: Int = 0
-    var degrees: Double = 0.0
-    fun velocityTest() {
-        konfetti.setOnTouchListener { _, event ->
-            when(event.action) {
-                MotionEvent.ACTION_DOWN -> {
-                    startX = event.x
-                    startY = event.y
-                }
-                MotionEvent.ACTION_MOVE -> {
-                    val dx = event.x - startX
-                    val dy = event.y - startY
-                    val r = Math.atan2(dy.toDouble(), dx.toDouble()) // In radians
-                    degrees = ((r * (180 / Math.PI) - 180) + 360) % 360
-
-                    val length = Math.sqrt((dx * dx) + (dy * dy).toDouble())
-                    speed = (length / 100).toInt()
-                    if(speed > 10) speed = 0
-                }
-                MotionEvent.ACTION_UP -> {
-                    val colors = intArrayOf(color(R.color.confetti1), color(R.color.confetti2), color(R.color.confetti3), color(R.color.confetti4))
-                    konfetti.build()
-                            .addColors(*colors)
-                            .setDirection(0.0, 359.0)
-                            .setSpeed(1f, 5f)
-                            .addShapes(Shape.RECT, Shape.CIRCLE)
-                            .addSizes(Size.SMALL)
-                            .setPosition(-50f, konfetti.width + 50f, -50f, -50f)
-                            .setTimeToLive(2500)
-                            .setFadeOutEnabled(true)
-                            .burst(Random().nextInt(500 + 10))
-                }
-            }
-            true
+    fun setupSectionButtons() {
+        buttonSectionBurstFromTop.setOnClickListener {
+            // TODO implement loading config for this section
+        }
+        buttonSectionDragAndBurst.setOnClickListener {
+            // TODO implement loading config for this section
+        }
+        buttonSectionStreamFromCenter.setOnClickListener {
+            // TODO implement loading config for this section
         }
     }
 
-    fun color(resId: Int): Int {
-        return ContextCompat.getColor(applicationContext, resId)
-    }
-
-    fun getTimerSeekBarChangeListener(): SeekBar.OnSeekBarChangeListener {
-        return object : SeekBar.OnSeekBarChangeListener {
-            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                if (konfetti.systems.isNotEmpty()) {
-                    konfetti.systems.forEach {
-                        timer = seekbarX.progress
-                        if (fromUser) {
-//                            it.setSpawnDelay(timer)
-                        }
-                    }
-                }
-            }
-
-            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
-
-            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
-        }
-    }
-
-    fun getWindSeekBarChangeListener(): SeekBar.OnSeekBarChangeListener {
-        return object : SeekBar.OnSeekBarChangeListener {
-            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                if (konfetti.systems.isNotEmpty()) {
-                    konfetti.systems.forEach {
-                        if (fromUser) {
-//                            it.setAngle(progress.toDouble())
-//                            it.setSpeed(10)
-                        }
-                    }
-                }
-            }
-
-            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
-
-            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
-        }
-    }
 }
