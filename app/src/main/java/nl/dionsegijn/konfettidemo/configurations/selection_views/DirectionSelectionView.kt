44e13584e1d334ee4124398876fb6d58787425c3
Dion Segijn <dion@blendle.com>
2017-05-22 16:02:51 +0200

    Implement DirectionConfigurationView

--- app/src/main/java/nl/dionsegijn/konfettidemo/configurations/selection_views/DirectionSelectionView.kt
+++ app/src/main/java/nl/dionsegijn/konfettidemo/configurations/selection_views/DirectionSelectionView.kt
@@ -1,6 +1,7 @@
 package nl.dionsegijn.konfettidemo.configurations.selection_views
 
 import android.content.Context
+import android.view.Gravity
 import android.widget.LinearLayout
 import nl.dionsegijn.konfettidemo.R
 
@@ -11,7 +12,9 @@ class DirectionSelectionView(context: Context?) : LinearLayout(context) {
 
     init {
         inflate(context, R.layout.view_section_direction_selection, this)
+        gravity = Gravity.CENTER
         orientation = HORIZONTAL
     }
+
 
 }
