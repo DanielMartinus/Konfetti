829ca54dc15c964472af949ed5809d22c6f5869e
Dion Segijn <dion@blendle.com>
2017-05-22 16:02:39 +0200

    Implement show simple circle with sliders as sliders on a wheel

--- app/src/main/java/nl/dionsegijn/konfettidemo/configurations/DirectionConfigurationView.kt
+++ app/src/main/java/nl/dionsegijn/konfettidemo/configurations/DirectionConfigurationView.kt
@@ -1,10 +1,11 @@
-package nl.dionsegijn.konfettidemo.configurations.selection_views
+package nl.dionsegijn.konfettidemo.configurations
 
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
 
 /**
@@ -12,31 +13,44 @@ import android.view.View
  */
 class DirectionConfigurationView : View {
 
-    var paintCircleStroke: Paint
+    val paintCircleStroke: Paint = Paint()
+    val paintCircleSolid: Paint = Paint()
 
     constructor(context: Context?) : super(context)
     constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
     constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
 
     init {
-        paintCircleStroke = Paint()
         paintCircleStroke.color = Color.RED
         paintCircleStroke.style = Paint.Style.STROKE
         paintCircleStroke.strokeWidth = 5f
+
+        paintCircleSolid.color = Color.RED
+        paintCircleSolid.strokeWidth = 5f
     }
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
 
-        if(measuredHeight ==  0 && measuredWidth == 0) return
+        if(measuredHeight ==  0 &&  measuredWidth == 0) return
 
-        canvas.drawCircle(measuredHeight / 2f, measuredWidth / 2f, 50f, paintCircleStroke)
+        val cx: Float = (measuredHeight / 2f)
+        val cy: Float = (measuredWidth / 2f)
+        val size: Float = 20f
+        val r: Float = cx - size
+
+        val r1 = Math.toRadians(90.0)
+        val r2 = Math.toRadians(270.0)
+
+        canvas.drawCircle(cx, cy, cx - size, paintCircleStroke)
+
+        val x1 = cx + r * Math.cos(r1)
+        val y1 = cy + r * Math.sin(r1)
+        val x2 = cx + r * Math.cos(r2)
+        val y2 = cy + r * Math.sin(r2)
+
+        canvas.drawCircle(x1.toFloat(), y1.toFloat(), size, paintCircleSolid)
+        canvas.drawCircle(x2.toFloat(), y2.toFloat(), size, paintCircleSolid)
     }
 
-    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        if(widthMeasureSpec > 0 || heightMeasureSpec > 0) {
-            invalidate()
-        }
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
-    }
 }
