ca1eca5ec92b19e9073b8a71112c6585cda4c867
Dion Segijn <dion@blendle.com>
2017-05-23 20:23:27 +0200

    Create NonSwipeableViewPager

---
+++ app/src/main/java/nl/dionsegijn/konfettidemo/widgets/NonSwipeableViewPager.kt
@@ -0,0 +1,25 @@
+package nl.dionsegijn.konfettidemo.widgets
+
+import android.content.Context
+import android.support.v4.view.ViewPager
+import android.util.AttributeSet
+import android.view.MotionEvent
+
+
+
+/**
+ * Created by dionsegijn on 5/23/17.
+ */
+class NonSwipeableViewPager : ViewPager {
+    constructor(context: Context?) : super(context)
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        return false
+    }
+
+    override fun onInterceptTouchEvent(event: MotionEvent): Boolean {
+        return false
+    }
+
+}
