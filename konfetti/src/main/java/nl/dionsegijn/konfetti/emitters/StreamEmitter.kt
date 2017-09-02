89d53b3743f31d5d90ff1077dccd6786f97d741d
Dion Segijn <dionsegijn@hotmail.com>
2017-09-02 21:37:44 +0200

    Fixes #20 create the right amount of particles per second

--- konfetti/src/main/java/nl/dionsegijn/konfetti/emitters/StreamEmitter.kt
+++ konfetti/src/main/java/nl/dionsegijn/konfetti/emitters/StreamEmitter.kt
@@ -5,7 +5,6 @@ import nl.dionsegijn.konfetti.models.LocationModule
 import nl.dionsegijn.konfetti.models.Shape
 import nl.dionsegijn.konfetti.models.Size
 import nl.dionsegijn.konfetti.modules.VelocityModule
-import java.util.concurrent.TimeUnit
 
 /**
  * Created by dionsegijn on 4/2/17.
@@ -17,47 +16,57 @@ class StreamEmitter(location: LocationModule, velocity: VelocityModule, sizes: A
     /** Keeping count of how many particles are created */
     private var particlesCreated = 0
     /** Max time in milliseconds allowed to emit */
-    internal var emittingTime: Long = 0
+    private var emittingTime: Long = 0
     /** Elapsed time in milliseconds */
-    internal var elapsedTime: Float = 0f
+    private var elapsedTime: Float = 0f
     /** Milliseconds per particle creation */
-    var amountPerMs: Long = 0L
+    private var amountPerMs: Float = 0f
+    /** Elapsed time in milliseconds */
+    private var createParticleMs: Float = 0f
 
     fun emit(particlesPerSecond: Int, emittingTime: Long = 0L, maxParticles: Int = -1): StreamEmitter {
         this.maxParticles = maxParticles
         this.emittingTime = emittingTime
-        amountPerMs = TimeUnit.NANOSECONDS.toMillis(1000L / particlesPerSecond)
+        amountPerMs = 1f / particlesPerSecond
+        addConfetti()
         return this
     }
 
     /**
      * If timer isn't started yet, set initial start time
-     * Create the first confetti immediately and update the last emit time
+     * Create the first confetti immediately and update the last emitting time
      */
     override fun createConfetti(deltaTime: Float) {
 
-        // if maxParticles is set and particlesCreated not within
-        // range of maxParticles stop emitting
+        // If maxParticles is set and amount of particles created is not within the range of
+        // maxParticles stop emitting
         if(maxParticles in 1..(particlesCreated - 1)) {
             return
         }
 
         // Check if particle should be created
-        if (deltaTime >= amountPerMs && elapsedTime < emittingTime) {
-            this.addConfetti()
+        if(createParticleMs > amountPerMs && !isTimeElapsed()) {
+            // Calculate how many particle  to create in the elapsed time
+            val amount: Int = (createParticleMs / amountPerMs).toInt()
+            (1..amount).forEach { this.addConfetti() }
+            // Reset timer and add left over time for the next cycle
+            createParticleMs %= amountPerMs
         }
+        createParticleMs += deltaTime
 
         elapsedTime += deltaTime * 1000
     }
+
+    private fun isTimeElapsed(): Boolean = if(emittingTime == 0L) false else elapsedTime > emittingTime
 
     /**
      * When time is up and all particles disappeared
      */
     override fun isDoneEmitting(): Boolean {
-        if(emittingTime > 0L) {
-            return elapsedTime >= emittingTime && particles.size == 0
+        return if(emittingTime > 0L) {
+            elapsedTime >= emittingTime && particles.size == 0
         } else {
-            return particles.size == 0
+            particles.size == 0
         }
     }
 
