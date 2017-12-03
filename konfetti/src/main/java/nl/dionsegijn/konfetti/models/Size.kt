d061253d8d22d65f10757b32757988e8a57c2188
Dion Segijn <dionsegijn@hotmail.com>
2017-12-03 13:58:03 +0100

    Validate the mass not to be zero

--- konfetti/src/main/java/nl/dionsegijn/konfetti/models/Size.kt
+++ konfetti/src/main/java/nl/dionsegijn/konfetti/models/Size.kt
@@ -5,9 +5,16 @@ import android.content.res.Resources
 /**
  * Created by dionsegijn on 3/26/17.
  * [sizeInDp] the size of the confetti in dip
- * [mass] each size can have its own mass for slightly different behavior
+ * [mass] each size can have its own mass for slightly different behavior. For example, the closer
+ * the mass is to zero the easier it will accelerate.
  */
-data class Size(val sizeInDp: Int, val mass: Float = 5f)
+data class Size(val sizeInDp: Int, val mass: Float = 5f) {
 
-val Size.sizeInPx: Float
-    get() = (this.sizeInDp * Resources.getSystem().displayMetrics.density)
+    internal val sizeInPx: Float
+        get() = sizeInDp * Resources.getSystem().displayMetrics.density
+
+    init {
+        require(mass != 0F) { "mass=$mass must be != 0" }
+    }
+
+}
