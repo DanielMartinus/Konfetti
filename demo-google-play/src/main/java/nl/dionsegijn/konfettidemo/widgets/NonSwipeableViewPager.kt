15d8b986e22906ad1e32d6b0a78833fa5555be1e
Dion Segijn <dionsegijn@hotmail.com>
2017-12-03 13:25:43 +0100

    Fix nullable context and use expression body

--- demo-google-play/src/main/java/nl/dionsegijn/konfettidemo/widgets/NonSwipeableViewPager.kt
+++ demo-google-play/src/main/java/nl/dionsegijn/konfettidemo/widgets/NonSwipeableViewPager.kt
@@ -11,15 +11,11 @@ import android.view.MotionEvent
  * Created by dionsegijn on 5/23/17.
  */
 class NonSwipeableViewPager : ViewPager {
-    constructor(context: Context?) : super(context)
-    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
 
-    override fun onTouchEvent(event: MotionEvent): Boolean {
-        return false
-    }
+    override fun onTouchEvent(event: MotionEvent): Boolean = false
 
-    override fun onInterceptTouchEvent(event: MotionEvent): Boolean {
-        return false
-    }
+    override fun onInterceptTouchEvent(event: MotionEvent): Boolean = false
 
 }
