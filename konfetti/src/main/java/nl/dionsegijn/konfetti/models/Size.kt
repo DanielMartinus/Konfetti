76ecbca7a167e1aedf17ce0c02ea72371e10b48d
Dion Segijn <dionsegijn@hotmail.com>
2017-12-03 14:06:02 +0100

    Rename size to sizeInDp

--- konfetti/src/main/java/nl/dionsegijn/konfetti/models/Size.kt
+++ konfetti/src/main/java/nl/dionsegijn/konfetti/models/Size.kt
@@ -4,10 +4,10 @@ import android.content.res.Resources
 
 /**
  * Created by dionsegijn on 3/26/17.
- * [size] the size of the confetti in dip
+ * [sizeInDp] the size of the confetti in dip
  * [mass] each size can have its own mass for slightly different behavior
  */
-data class Size(val size: Int, val mass: Float = 5f)
+data class Size(val sizeInDp: Int, val mass: Float = 5f)
 
 val Size.sizeDp: Int
-    get() = (this.size * Resources.getSystem().displayMetrics.density).toInt()
+    get() = (this.sizeInDp * Resources.getSystem().displayMetrics.density).toInt()
