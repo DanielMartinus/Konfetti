bba8bbc9090d18fa1ff31c93f57d2aee3dfc33e1
Dion Segijn <dionsegijn@hotmail.com>
2018-01-20 17:48:59 +0100

    Fail test

--- konfetti/src/test/java/nl/dionsegijn/konfetti/Emitter/BurstEmitterTests.kt
+++ konfetti/src/test/java/nl/dionsegijn/konfetti/Emitter/BurstEmitterTests.kt
@@ -27,7 +27,7 @@ class BurstEmitterTests {
         burstEmitter.build(amount)
 
         burstEmitter.createConfetti(1f)
-        Mockito.verify(mockInvokeMethodClass, Mockito.times(amount)).invokeMethod()
+        Mockito.verify(mockInvokeMethodClass, Mockito.times(1)).invokeMethod()
 
         // Amount should not have changed, burst emitter only
         // create the particles one time after calling createConfetti
