20027fd241a5fc5ed68a3c03f371d2c2e56ab81a
Dion Segijn <dionsegijn@hotmail.com>
2020-09-12 10:42:59 +0200

    Add custom drawable example

--- demo-simple-java/src/main/java/nl/dionsegijn/simple_demo/MainActivity.java
+++ demo-simple-java/src/main/java/nl/dionsegijn/simple_demo/MainActivity.java
@@ -1,8 +1,11 @@
 package nl.dionsegijn.simple_demo;
 
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
 import android.view.View;
 
 import nl.dionsegijn.konfetti.KonfettiView;
@@ -16,6 +19,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final Drawable drawable = ContextCompat.getDrawable(getApplicationContext(), R.drawable.ic_heart);
+        final Shape.DrawableShape drawableShape = new Shape.DrawableShape(drawable, true);
+
         final KonfettiView konfettiView = findViewById(R.id.konfettiView);
         konfettiView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
                         .setSpeed(1f, 5f)
                         .setFadeOutEnabled(true)
                         .setTimeToLive(2000L)
-                        .addShapes(Shape.Square.INSTANCE, Shape.Circle.INSTANCE)
+                        .addShapes(Shape.Square.INSTANCE, Shape.Circle.INSTANCE, drawableShape)
                         .addSizes(new Size(12, 5f))
                         .setPosition(-50f, konfettiView.getWidth() + 50f, -50f, -50f)
                         .streamFor(300, 5000L);
