5054590a5da99a39d90915ab3beac42d4a8d1002
Dion Segijn <dionsegijn@hotmail.com>
2021-08-13 18:47:47 +0200

    Rename TimeToLiveSelectionView

--- demo-google-play/src/main/java/nl/dionsegijn/konfettidemo/configurations/ConfigurationControlsWidget.kt
+++ demo-google-play/src/main/java/nl/dionsegijn/konfettidemo/configurations/ConfigurationControlsWidget.kt
@@ -100,7 +100,7 @@ class ConfigurationControlsWidget : LinearLayout, OnConfigurationChangedListener
                 TabConfig(R.drawable.ic_paint, ColorSelectionView(context, configuration)),
                 TabConfig(R.drawable.ic_shapes, ShapeSelectionView(context, configuration)),
                 TabConfig(R.drawable.ic_speed, MultiSeekbarSelectionView(context, configuration, "Speed", 10, speedValuesChanged())),
-                TabConfig(R.drawable.ic_time_to_live, SeekbarSelectionView(context, configuration, "Time to live", 5000)),
+                TabConfig(R.drawable.ic_time_to_live, TimeToLiveSelectionView(context, configuration, "Time to live", 5000)),
                 TabConfig(R.drawable.ic_settings, GlobalConfigSelectionView(context, this, configuration)))
     }
 
