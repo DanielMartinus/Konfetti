428686e2e71bd56dcde5b1d1ba1b091212621130
Dion Segijn <dionsegijn@hotmail.com>
2021-12-07 21:53:32 +0100

    Make konfetti play store demo work with partysystem and party

--- demo-google-play/src/main/java/nl/dionsegijn/konfettidemo/MainActivity.kt
+++ demo-google-play/src/main/java/nl/dionsegijn/konfettidemo/MainActivity.kt
@@ -3,21 +3,26 @@ package nl.dionsegijn.konfettidemo
 import android.animation.Animator
 import android.animation.AnimatorListenerAdapter
 import android.animation.ValueAnimator
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.os.Handler
-import com.google.android.material.bottomsheet.BottomSheetBehavior
-import com.google.android.material.tabs.TabLayout
-import androidx.core.content.ContextCompat
-import androidx.appcompat.app.AppCompatActivity
 import android.view.MotionEvent
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.tabs.TabLayout
+import nl.dionsegijn.konfetti_core._new.NewEmitter.EmitterBase
+import nl.dionsegijn.konfetti_core._new.Party
+import nl.dionsegijn.konfetti_core._new.PartyEmitter.PartyEmitter
+import nl.dionsegijn.konfetti_core._new.Position
 import nl.dionsegijn.konfetti_core.models.Shape
-import nl.dionsegijn.konfetti_core.models.Size
 import nl.dionsegijn.konfettidemo.configurations.settings.Configuration
 import nl.dionsegijn.konfettidemo.configurations.settings.ConfigurationManager
 import nl.dionsegijn.konfettidemo.databinding.ActivityMainBinding
 import nl.dionsegijn.konfettidemo.interfaces.OnConfigurationChangedListener
 import nl.dionsegijn.konfettidemo.interfaces.OnSimpleTabSelectedListener
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity(), OnConfigurationChangedListener {
 
@@ -84,39 +89,51 @@ class MainActivity : AppCompatActivity(), OnConfigurationChangedListener {
 
     private fun streamFromTop(config: Configuration, colors: IntArray) {
         if (!canIHaveMoreConfetti()) return
-        binding.viewKonfetti.build()
-            .addColors(*colors)
-            .setDirection(0.0, 359.0)
-            .setSpeed(config.minSpeed, config.maxSpeed)
-            .setFadeOutEnabled(true)
-            .setTimeToLive(config.timeToLive)
-            .addShapes(*config.shapes)
-            .addSizes(Size(12), Size(16, 6f))
-            .setPosition(-50f, binding.viewKonfetti.width + 50f, -50f, -50f)
-            .streamFor(300, 5000L)
+        binding.viewKonfetti.start(
+            Party(
+                spread = 359,
+                colors = colors.toList(),
+                startVelocity = config.minSpeed.toInt(),
+                timeToLive = config.timeToLive,
+                shapes = config.shapes.toList(),
+                position = Position(binding.viewKonfetti.width / 2f, -50f),
+                emitter = EmitterBase(5L, TimeUnit.SECONDS).max(300)
+            )
+        )
+//            .addColors(*colors)
+//            .setDirection(0.0, 359.0)
+//            .setSpeed(config.minSpeed, config.maxSpeed)
+//            .setFadeOutEnabled(true)
+//            .setTimeToLive(config.timeToLive)
+//            .addShapes(*config.shapes)
+//            .addSizes(Size(12), Size(16, 6f))
+//            .setPosition(-50f, binding.viewKonfetti.width + 50f, -50f, -50f)
+//            .streamFor(300, 5000L)
     }
 
     private fun burstFromCenter(config: Configuration, colors: IntArray) {
         if (!canIHaveMoreConfetti()) return
-        binding.viewKonfetti.build()
-            .addColors(*colors)
-            .setDirection(0.0, 359.0)
-            .setSpeed(config.minSpeed, config.maxSpeed)
-            .setFadeOutEnabled(true)
-            .setTimeToLive(config.timeToLive)
-            .addShapes(*config.shapes)
-            .addSizes(Size(12), Size(16, 6f))
-            .setPosition(
-                binding.viewKonfetti.x + binding.viewKonfetti.width / 2,
-                binding.viewKonfetti.y + binding.viewKonfetti.height / 3
+        binding.viewKonfetti.start(
+            Party(
+                spread = 359,
+                colors = colors.toList(),
+                startVelocity = (config.minSpeed.toInt() + config.maxSpeed.toInt()) / 2,
+                timeToLive = config.timeToLive,
+                shapes = config.shapes.toList(),
+                position = Position(
+                    binding.viewKonfetti.x + binding.viewKonfetti.width / 2,
+                    binding.viewKonfetti.y + binding.viewKonfetti.height / 3
+                ),
+                emitter = EmitterBase(duration = 300L).max(100)
             )
-            .burst(100)
+        )
     }
 
     private var startX: Float = 0f
     private var startY: Float = 0f
     private var speed: Int = 0
     private var degrees: Double = 0.0
+    @SuppressLint("ClickableViewAccessibility")
     private fun velocityTest() {
         binding.viewKonfetti.setOnTouchListener { _, event ->
             val modeEnabled =
@@ -141,16 +158,18 @@ class MainActivity : AppCompatActivity(), OnConfigurationChangedListener {
                     val colors =
                         binding.viewConfigurationControls.configuration.active.colors.map { color(it) }
                             .toIntArray()
-                    binding.viewKonfetti.build()
-                        .addColors(*colors)
-                        .setDirection(degrees - 50, degrees + 50)
-                        .setSpeed(0f, speed + 5f)
-                        .addShapes(Shape.Square, Shape.Circle)
-                        .addSizes(Size(12), Size(16, 6f))
-                        .setPosition(startX, startY)
-                        .setTimeToLive(10000)
-                        .setFadeOutEnabled(true)
-                        .burst(200)
+                    binding.viewKonfetti.start(
+                        Party(
+                            angle = degrees.toInt(),
+                            spread = 30,
+                            colors = colors.toList(),
+                            startVelocity = speed,
+                            timeToLive = 10000L,
+                            shapes = listOf(Shape.Square, Shape.Circle),
+                            position = Position(startX, startY),
+                            emitter = PartyEmitter.burst(100)
+                        )
+                    )
                 }
             }
             return@setOnTouchListener modeEnabled
@@ -185,9 +204,10 @@ class MainActivity : AppCompatActivity(), OnConfigurationChangedListener {
 
     private fun updateSystemsInfo() {
         val activeSystems = binding.viewKonfetti.getActiveSystems()
-        val activeParticles = activeSystems.sumBy { it.activeParticles() }
+        // TODO update activeParticles
+//        val activeParticles = activeSystems.sumBy { it.activeParticles() }
         binding.viewSystemInfo.text =
-            "Active systems: ${activeSystems.size} \nActive particles: $activeParticles"
+            "Active systems: ${activeSystems.size} \nActive particles: N/A"
     }
 
     /**
