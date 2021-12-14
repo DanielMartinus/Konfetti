9c3076093f52a4424ef1cc921d178bd76f7c916c
Dion Segijn <dionsegijn@hotmail.com>
2021-12-14 20:02:15 +0100

    Remove commented code

--- demo-simple-java/src/main/java/nl/dionsegijn/simple_demo/MainActivity.java
+++ demo-simple-java/src/main/java/nl/dionsegijn/simple_demo/MainActivity.java
@@ -37,17 +37,5 @@ public class MainActivity extends AppCompatActivity {
         konfettiView.setOnClickListener(view ->
                 konfettiView.start(party)
         );
-
-
-//                konfettiView.build()
-//                .addColors(Color.YELLOW, Color.GREEN, Color.MAGENTA)
-//                .setDirection(0.0, 359.0)
-//                .setSpeed(1f, 5f)
-//                .setFadeOutEnabled(true)
-//                .setTimeToLive(2000L)
-//                .addShapes(new Shape.Rectangle(0.2f), drawableShape)
-//                .addSizes(new Size(12, 5f))
-//                .setPosition(-50f, konfettiView.getWidth() + 50f, -50f, -50f)
-//                .streamFor(300, 5000L));
     }
 }
