a6b456481e24d113bbb1e1861e5f34dcb4237d4b
Dion Segijn <dionsegijn@hotmail.com>
2021-12-18 12:34:23 +0100

    Try randomness for natural feeling

--- demo-compose/src/main/java/nl/dionsegijn/demo_compose/KonfettiViewModel.kt
+++ demo-compose/src/main/java/nl/dionsegijn/demo_compose/KonfettiViewModel.kt
@@ -8,7 +8,6 @@ import nl.dionsegijn.konfetti_core.Angle
 import nl.dionsegijn.konfetti_core.NewEmitter.Emitter
 import nl.dionsegijn.konfetti_core.Party
 import nl.dionsegijn.konfetti_core.Position
-import nl.dionsegijn.konfetti_core.Spread
 import nl.dionsegijn.konfetti_core.models.Size
 import java.util.concurrent.TimeUnit
 
@@ -19,26 +18,39 @@ class KonfettiViewModel : ViewModel() {
 
     fun start(drawable: Drawable) {
         val party = Party(
-            speed = 45f,
+            speed = 30f,
+            maxSpeed = 50f,
             damping = 0.9f,
             angle = Angle.TOP,
-            spread = Spread.SMALL,
+            spread = 45,
             size = listOf(Size.MEDIUM),
             timeToLive = 3000L,
-            colors = listOf(0xfce18a, 0xff726d),
-            emitter = Emitter(duration = 200, TimeUnit.MILLISECONDS).max(100),
+            colors = listOf(0xfce18a, 0xff726d, 0xf4306d, 0xb48def),
+            emitter = Emitter(duration = 100, TimeUnit.MILLISECONDS).max(30),
             position = Position.relative(0.5, 1.0)
         )
 
         _state.value = State.Started(
             listOf(
                 party,
-//                party.copy(
-//                    speed = 40f,
-//                    maxSpeed = 45f,
-//                    spread = Spread.SMALL - 5,
-//                    colors = listOf(0xf4306d, 0xb48def),
-//                )
+                party.copy(
+                    speed = 55f,
+                    maxSpeed = 65f,
+                    spread = 10,
+                    emitter = Emitter(duration = 100, TimeUnit.MILLISECONDS).max(10),
+                ),
+                party.copy(
+                    speed = 50f,
+                    maxSpeed = 60f,
+                    spread = 120,
+                    emitter = Emitter(duration = 100, TimeUnit.MILLISECONDS).max(40),
+                ),
+                party.copy(
+                    speed = 65f,
+                    maxSpeed = 80f,
+                    spread = 10,
+                    emitter = Emitter(duration = 100, TimeUnit.MILLISECONDS).max(10),
+                ),
             )
         )
     }
