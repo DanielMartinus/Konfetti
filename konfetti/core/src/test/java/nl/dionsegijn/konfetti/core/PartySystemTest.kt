1fdab36144559bae2090da19961f4c4c5b4a3951
Dion Segijn <dionsegijn@hotmail.com>
2021-12-28 10:16:30 +0100

    Add test disabling generating particles

--- konfetti/core/src/test/java/nl/dionsegijn/konfetti/core/PartySystemTest.kt
+++ konfetti/core/src/test/java/nl/dionsegijn/konfetti/core/PartySystemTest.kt
@@ -47,7 +47,32 @@ class PartySystemTest {
         Assert.assertTrue(system.enabled)
         Assert.assertFalse(system.isDoneEmitting())
 
-        val particles = system.render(60f, rect)
-        Assert.assertEquals(2, particles.size)
+        val r1 = system.render(60f, rect)
+        Assert.assertEquals(2, r1.size)
+    }
+
+    @Test
+    fun `Test PartySystem set to disabled stops generating particles`() {
+        val party = Party(
+            emitter = Emitter(100L).max(4)
+        )
+        val system = PartySystem(party, pixelDensity = 1f)
+
+        Assert.assertTrue(system.enabled)
+        Assert.assertFalse(system.isDoneEmitting())
+
+        val r1 = system.render(deltaTime, rect) // render 2, total deltaTime = 0.017f
+        Assert.assertEquals(0, r1.size) // Expected 0, Every 0.025ms a new particle should be created
+
+        val r2 = system.render(deltaTime, rect) // render 2, total deltaTime = 2 * 0.017f = 0.034f
+        Assert.assertEquals(1, r2.size) // Expected 1, one for every 0.025ms
+
+        system.enabled = false
+        Assert.assertFalse(system.enabled)
+
+        // Should not longer create new particles even though time has passed
+        val r3 = system.render(deltaTime, rect)
+        Assert.assertEquals(1, r3.size)
+    }
     }
 }
