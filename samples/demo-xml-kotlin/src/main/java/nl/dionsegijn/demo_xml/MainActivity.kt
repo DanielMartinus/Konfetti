5e54cd37a8b790af2923082d34d26e356287640c
Dion Segijn <dionsegijn@hotmail.com>
2021-12-28 20:54:11 +0100

    Utilize presets from Shared module in xml-kotlin demo

--- samples/demo-xml-kotlin/src/main/java/nl/dionsegijn/demo_xml/MainActivity.kt
+++ samples/demo-xml-kotlin/src/main/java/nl/dionsegijn/demo_xml/MainActivity.kt
@@ -3,15 +3,8 @@ package nl.dionsegijn.demo_xml
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import nl.dionsegijn.konfetti.core.Angle
-import nl.dionsegijn.konfetti.core.Party
-import nl.dionsegijn.konfetti.core.Position
-import nl.dionsegijn.konfetti.core.Rotation
-import nl.dionsegijn.konfetti.core.Spread
-import nl.dionsegijn.konfetti.core.emitter.Emitter
-import nl.dionsegijn.konfetti.core.models.Size
 import nl.dionsegijn.konfetti.xml.KonfettiView
-import java.util.concurrent.TimeUnit
+import nl.dionsegijn.samples.shared.Presets
 
 class MainActivity : AppCompatActivity() {
 
@@ -29,94 +22,30 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun festive() {
-        val party = Party(
-            speed = 30f,
-            maxSpeed = 50f,
-            damping = 0.9f,
-            angle = Angle.TOP,
-            spread = 45,
-            size = listOf(Size.SMALL, Size.LARGE),
-            timeToLive = 3000L,
-            rotation = Rotation(),
-            colors = listOf(0xfce18a, 0xff726d, 0xf4306d, 0xb48def),
-            emitter = Emitter(duration = 100, TimeUnit.MILLISECONDS).max(30),
-            position = Position.Relative(0.5, 1.0)
-        )
-
-        viewKonfetti.start(
-            listOf(
-                party,
-                party.copy(
-                    speed = 55f,
-                    maxSpeed = 65f,
-                    spread = 10,
-                    emitter = Emitter(duration = 100, TimeUnit.MILLISECONDS).max(10),
-                ),
-                party.copy(
-                    speed = 50f,
-                    maxSpeed = 60f,
-                    spread = 120,
-                    emitter = Emitter(duration = 100, TimeUnit.MILLISECONDS).max(40),
-                ),
-                party.copy(
-                    speed = 65f,
-                    maxSpeed = 80f,
-                    spread = 10,
-                    emitter = Emitter(duration = 100, TimeUnit.MILLISECONDS).max(10),
-                )
-            )
-        )
+        /**
+         * See [Presets] for this configuration
+         */
+        viewKonfetti.start(Presets.festive())
     }
 
     private fun explode() {
-        viewKonfetti.start(
-            Party(
-                speed = 0f,
-                maxSpeed = 30f,
-                damping = 0.9f,
-                spread = 360,
-                colors = listOf(0xfce18a, 0xff726d, 0xf4306d, 0xb48def),
-                emitter = Emitter(duration = 100, TimeUnit.MILLISECONDS).max(100),
-                position = Position.Relative(0.5, 0.3)
-            )
-        )
+        /**
+         * See [Presets] for this configuration
+         */
+        viewKonfetti.start(Presets.explode())
     }
 
     private fun parade() {
-        val party = Party(
-            speed = 10f,
-            maxSpeed = 30f,
-            damping = 0.9f,
-            angle = Angle.RIGHT - 45,
-            spread = Spread.SMALL,
-            colors = listOf(0xfce18a, 0xff726d, 0xf4306d, 0xb48def),
-            emitter = Emitter(duration = 5, TimeUnit.SECONDS).perSecond(30),
-            position = Position.Relative(0.0, 0.5)
-        )
-
-        viewKonfetti.start(
-            listOf(
-                party,
-                party.copy(
-                    angle = party.angle - 90, // flip angle from right to left
-                    position = Position.Relative(1.0, 0.5)
-                ),
-            )
-        )
+        /**
+         * See [Presets] for this configuration
+         */
+        viewKonfetti.start(Presets.parade())
     }
 
     private fun rain() {
-        viewKonfetti.start(
-            Party(
-                speed = 0f,
-                maxSpeed = 15f,
-                damping = 0.9f,
-                angle = Angle.BOTTOM,
-                spread = Spread.ROUND,
-                colors = listOf(0xfce18a, 0xff726d, 0xf4306d, 0xb48def),
-                emitter = Emitter(duration = 5, TimeUnit.SECONDS).perSecond(100),
-                position = Position.Relative(0.0, 0.0).between(Position.Relative(1.0, 0.0))
-            )
-        )
+        /**
+         * See [Presets] for this configuration
+         */
+        viewKonfetti.start(Presets.rain())
     }
 }
