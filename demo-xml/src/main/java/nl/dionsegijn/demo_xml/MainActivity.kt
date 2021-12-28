5d67007a1e10514053c691d726285accab454317
Dion Segijn <dionsegijn@hotmail.com>
2021-12-28 17:36:48 +0100

    Add demo-xml module

---
+++ demo-xml/src/main/java/nl/dionsegijn/demo_xml/MainActivity.kt
@@ -0,0 +1,122 @@
+package nl.dionsegijn.demo_xml
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import nl.dionsegijn.konfetti.core.Angle
+import nl.dionsegijn.konfetti.core.Party
+import nl.dionsegijn.konfetti.core.Position
+import nl.dionsegijn.konfetti.core.Rotation
+import nl.dionsegijn.konfetti.core.Spread
+import nl.dionsegijn.konfetti.core.emitter.Emitter
+import nl.dionsegijn.konfetti.core.models.Size
+import nl.dionsegijn.konfetti.xml.KonfettiView
+import java.util.concurrent.TimeUnit
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewKonfetti: KonfettiView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        viewKonfetti = findViewById(R.id.konfettiView)
+        findViewById<Button>(R.id.btnFestive).setOnClickListener { festive() }
+        findViewById<Button>(R.id.btnExplode).setOnClickListener { explode() }
+        findViewById<Button>(R.id.btnParade).setOnClickListener { parade() }
+        findViewById<Button>(R.id.btnRain).setOnClickListener { rain() }
+    }
+
+    private fun festive() {
+        val party = Party(
+            speed = 30f,
+            maxSpeed = 50f,
+            damping = 0.9f,
+            angle = Angle.TOP,
+            spread = 45,
+            size = listOf(Size.SMALL, Size.LARGE),
+            timeToLive = 3000L,
+            rotation = Rotation(),
+            colors = listOf(0xfce18a, 0xff726d, 0xf4306d, 0xb48def),
+            emitter = Emitter(duration = 100, TimeUnit.MILLISECONDS).max(30),
+            position = Position.Relative(0.5, 1.0)
+        )
+
+        viewKonfetti.start(
+            listOf(
+                party,
+                party.copy(
+                    speed = 55f,
+                    maxSpeed = 65f,
+                    spread = 10,
+                    emitter = Emitter(duration = 100, TimeUnit.MILLISECONDS).max(10),
+                ),
+                party.copy(
+                    speed = 50f,
+                    maxSpeed = 60f,
+                    spread = 120,
+                    emitter = Emitter(duration = 100, TimeUnit.MILLISECONDS).max(40),
+                ),
+                party.copy(
+                    speed = 65f,
+                    maxSpeed = 80f,
+                    spread = 10,
+                    emitter = Emitter(duration = 100, TimeUnit.MILLISECONDS).max(10),
+                )
+            )
+        )
+    }
+
+    private fun explode() {
+        viewKonfetti.start(
+            Party(
+                speed = 0f,
+                maxSpeed = 30f,
+                damping = 0.9f,
+                spread = 360,
+                colors = listOf(0xfce18a, 0xff726d, 0xf4306d, 0xb48def),
+                emitter = Emitter(duration = 100, TimeUnit.MILLISECONDS).max(100),
+                position = Position.Relative(0.5, 0.3)
+            )
+        )
+    }
+
+    private fun parade() {
+        val party = Party(
+            speed = 10f,
+            maxSpeed = 30f,
+            damping = 0.9f,
+            angle = Angle.RIGHT - 45,
+            spread = Spread.SMALL,
+            colors = listOf(0xfce18a, 0xff726d, 0xf4306d, 0xb48def),
+            emitter = Emitter(duration = 5, TimeUnit.SECONDS).perSecond(30),
+            position = Position.Relative(0.0, 0.5)
+        )
+
+        viewKonfetti.start(
+            listOf(
+                party,
+                party.copy(
+                    angle = party.angle - 90, // flip angle from right to left
+                    position = Position.Relative(1.0, 0.5)
+                ),
+            )
+        )
+    }
+
+    private fun rain() {
+        viewKonfetti.start(
+            Party(
+                speed = 0f,
+                maxSpeed = 15f,
+                damping = 0.9f,
+                angle = Angle.BOTTOM,
+                spread = Spread.ROUND,
+                colors = listOf(0xfce18a, 0xff726d, 0xf4306d, 0xb48def),
+                emitter = Emitter(duration = 5, TimeUnit.SECONDS).perSecond(100),
+                position = Position.Relative(0.0, 0.0).between(Position.Relative(1.0, 0.0))
+            )
+        )
+    }
+}
