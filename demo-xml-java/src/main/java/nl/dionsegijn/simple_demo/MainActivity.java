1e76c7a3b2c5a75f500a019d62c75fb7374b3135
Dion Segijn <dionsegijn@hotmail.com>
2021-12-28 19:57:20 +0100

    Implement java xml samples

--- demo-xml-java/src/main/java/nl/dionsegijn/simple_demo/MainActivity.java
+++ demo-xml-java/src/main/java/nl/dionsegijn/simple_demo/MainActivity.java
@@ -1,22 +1,30 @@
 package nl.dionsegijn.simple_demo;
 
+import static nl.dionsegijn.konfetti.core.Position.Relative;
+
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
-import nl.dionsegijn.konfetti.xml.KonfettiView;
-import nl.dionsegijn.konfetti.core.emitter.Emitter;
-import nl.dionsegijn.konfetti.core.emitter.EmitterConfig;
+import nl.dionsegijn.konfetti.core.Angle;
 import nl.dionsegijn.konfetti.core.Party;
 import nl.dionsegijn.konfetti.core.PartyFactory;
+import nl.dionsegijn.konfetti.core.Spread;
+import nl.dionsegijn.konfetti.core.emitter.Emitter;
+import nl.dionsegijn.konfetti.core.emitter.EmitterConfig;
 import nl.dionsegijn.konfetti.core.models.Shape;
 import nl.dionsegijn.konfetti.core.models.Size;
+import nl.dionsegijn.konfetti.xml.KonfettiView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private KonfettiView konfettiView = null;
+    private Shape.DrawableShape drawableShape = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,9 +32,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         final Drawable drawable = ContextCompat.getDrawable(getApplicationContext(), R.drawable.ic_heart);
-        final Shape.DrawableShape drawableShape = new Shape.DrawableShape(drawable, true);
+        drawableShape = new Shape.DrawableShape(drawable, true);
 
-        final KonfettiView konfettiView = findViewById(R.id.konfettiView);
+        konfettiView = findViewById(R.id.konfettiView);
         EmitterConfig emitterConfig = new Emitter(5L, TimeUnit.SECONDS).perSecond(50);
         Party party = new PartyFactory(emitterConfig)
                 .angle(270)
@@ -39,6 +47,61 @@ public class MainActivity extends AppCompatActivity {
                 .build();
         konfettiView.setOnClickListener(view ->
                 konfettiView.start(party)
+        );
+
+        findViewById(R.id.btnExplode).setOnClickListener(v -> {
+            explode();
+        });
+        findViewById(R.id.btnParade).setOnClickListener(v -> {
+            parade();
+        });
+        findViewById(R.id.btnRain).setOnClickListener(v -> {
+            rain();
+        });
+    }
+
+    public void explode() {
+        EmitterConfig emitterConfig = new Emitter(100L, TimeUnit.MILLISECONDS).max(100);
+        konfettiView.start(
+                new PartyFactory(emitterConfig)
+                        .spread(360)
+                        .colors(Arrays.asList(0xfce18a, 0xff726d, 0xf4306d, 0xb48def))
+                        .setSpeedBetween(0f, 30f)
+                        .position(new Relative(0.5, 0.3))
+                        .build()
+        );
+    }
+
+    public void parade() {
+        EmitterConfig emitterConfig = new Emitter(5, TimeUnit.SECONDS).perSecond(30);
+        konfettiView.start(
+                new PartyFactory(emitterConfig)
+                        .angle(Angle.RIGHT - 45)
+                        .spread(Spread.SMALL)
+                        .colors(Arrays.asList(0xfce18a, 0xff726d, 0xf4306d, 0xb48def))
+                        .setSpeedBetween(10f, 30f)
+                        .position(new Relative(0.0, 0.5))
+                        .build(),
+                new PartyFactory(emitterConfig)
+                        .angle(Angle.LEFT + 45)
+                        .spread(Spread.SMALL)
+                        .colors(Arrays.asList(0xfce18a, 0xff726d, 0xf4306d, 0xb48def))
+                        .setSpeedBetween(10f, 30f)
+                        .position(new Relative(1.0, 0.5))
+                        .build()
+        );
+    }
+
+    public void rain() {
+        EmitterConfig emitterConfig = new Emitter(5, TimeUnit.SECONDS).perSecond(100);
+        konfettiView.start(
+                new PartyFactory(emitterConfig)
+                        .angle(Angle.BOTTOM)
+                        .spread(Spread.ROUND)
+                        .colors(Arrays.asList(0xfce18a, 0xff726d, 0xf4306d, 0xb48def))
+                        .setSpeedBetween(0f, 15f)
+                        .position(new Relative(0.0, 0.0).between(new Relative(1.0, 0.0)))
+                        .build()
         );
     }
 }
