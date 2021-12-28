3f789c05ea4745f78c699aff1cb2d4b9479c797e
Dion Segijn <dionsegijn@hotmail.com>
2021-12-28 10:15:51 +0100

    Improve test rendering different times and showing clear results

--- konfetti/core/src/test/java/nl/dionsegijn/konfetti/core/PartySystemTest.kt
+++ konfetti/core/src/test/java/nl/dionsegijn/konfetti/core/PartySystemTest.kt
@@ -14,18 +14,27 @@ class PartySystemTest {
         Mockito.`when`(contains(anyInt(), anyInt())).thenReturn(true)
     }
 
+    // Average between for each frame
+    private val deltaTime = 0.017f
+
     @Test
-    fun `Test creating particle after 50ms`() {
+    fun `Test creating particle every 25ms`() {
         val party = Party(
-            emitter = Emitter(100L).max(2)
+            emitter = Emitter(100L).max(4)
         )
         val system = PartySystem(party, pixelDensity = 1f)
 
         Assert.assertTrue(system.enabled)
         Assert.assertFalse(system.isDoneEmitting())
 
-        val render1 = system.render(0.05f, rect)
-        Assert.assertEquals(1, render1.size)
+        val r1 = system.render(deltaTime, rect) // render 2, total deltaTime = 0.017f
+        Assert.assertEquals(0, r1.size) // Expected 0, Every 0.025ms a new particle should be created
+
+        val r2 = system.render(deltaTime, rect) // render 2, total deltaTime = 2 * 0.017f = 0.034f
+        Assert.assertEquals(1, r2.size) // Expected 1, one for every 0.025ms
+
+        val r3 = system.render(deltaTime, rect) // render 3, total deltaTime = 3 * 0.017f = 0.051f
+        Assert.assertEquals(2, r3.size) // expected 2, one for every 0.025ms
     }
 
     @Test
