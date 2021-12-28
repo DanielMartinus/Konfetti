18673dc7a5412e1244a0f58c5be00e92dd25b1b4
Dion Segijn <dionsegijn@hotmail.com>
2021-12-28 17:43:42 +0100

    Remove demo-google-play demo
    No longer maintained on Google Play and the code is too complex for a demo. I replaced it with a new demo project showing four simple cases

--- demo-google-play/src/main/java/nl/dionsegijn/konfettidemo/configurations/viewpager/TabConfig.kt
+++
@@ -1,8 +0,0 @@
-package nl.dionsegijn.konfettidemo.configurations.viewpager
-
-import android.view.View
-
-/**
- * Created by dionsegijn on 5/24/17.
- */
-class TabConfig(val icon: Int, val widgetView: View)
