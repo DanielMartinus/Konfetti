ebe36b4f11ac29a372dd460ff60d77a2c2eda59e
Dion Segijn <dionsegijn@hotmail.com>
2023-07-15 13:49:48 +0200

    Migrate xml-java

--- samples/xml-java/build.gradle.kts
+++ samples/xml-java/build.gradle.kts
@@ -1,31 +1,33 @@
-apply plugin: "com.android.application"
+plugins {
+    id("com.android.application")
+}
 
 android {
-    compileSdkVersion = 33
+    compileSdk = 33
     buildToolsVersion = "34.0.0"
 
     defaultConfig {
         minSdk = 17
-        targetSdk 33
+        targetSdk = 33
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
     }
     buildTypes {
         release {
-            minifyEnabled = false
+            isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
         }
     }
     namespace = "nl.dionsegijn.xml.java"
     lint {
         abortOnError = true
-        baseline(file("lint-baseline.xml"))
+        baseline = file("lint-baseline.xml")
     }
 }
 
 dependencies {
-    implementation(project(path: ":konfetti:xml"))
+    implementation(project(path = ":konfetti:xml"))
     implementation("androidx.appcompat:appcompat:1.4.1")
     implementation("com.google.android.material:material:1.5.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.3")
