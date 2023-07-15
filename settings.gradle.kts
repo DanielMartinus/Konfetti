b0be27d42a6ba79dbbcff64998d31cf00b3d1dba
Dion Segijn <dionsegijn@hotmail.com>
2023-07-15 12:00:25 +0200

    Convert settings.gradle to .kts

---
+++ settings.gradle.kts
@@ -0,0 +1,13 @@
+// Library modules
+include(
+    ":konfetti:xml",
+    ":konfetti:compose",
+    ":konfetti:core"
+)
+// Sample modules
+include(
+    ":samples:compose-kotlin",
+    ":samples:xml-kotlin",
+    ":samples:xml-java",
+    ":samples:shared"
+)
