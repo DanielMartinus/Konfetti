545b4c5c45dd71ac8f05aa939d6b975cd8fc2ec5
Dion Segijn <dionsegijn@hotmail.com>
2023-12-28 15:12:18 +0100

    Make gradle file jvm only

--- konfetti/core/build.gradle.kts
+++ konfetti/core/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.library")
-    id("kotlin-android")
+    id("java-library")
+    id("kotlin")
     id("com.diffplug.spotless")
 }
 
@@ -9,41 +9,13 @@ apply(from = "../../scripts/publish-module.gradle.kts")
 
 spotless {
     kotlin {
-        ktlint("0.37.2")
+        ktlint("1.1.0")
         target("src/**/*.kt")
     }
     java {
         removeUnusedImports()
-        googleJavaFormat("1.5")
+        googleJavaFormat("1.15.0")
         target("**/*.java")
-    }
-}
-
-android {
-    compileSdk = buildVersions.compileSdk
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-
-    defaultConfig {
-        minSdk = 15
-    }
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
-        }
-    }
-    namespace = "nl.dionsegijn.konfetti.core"
-    lint {
-        abortOnError = true
-        baseline = file("lint-baseline.xml")
     }
 }
 
@@ -51,5 +23,4 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib:${Constants.kotlinVersion}")
     testImplementation(libs.test.junit)
     testImplementation(libs.test.mockito)
-
 }
